40fc7d7019a7f4203e3a070582fd2c322f0461c5
chujmnieto <92150561+chujmnieto@users.noreply.github.com>
2022-03-23 16:24:23 +0100

    .

--- src/Photos.js
+++ src/Photos.js
@@ -12,7 +12,7 @@ export default function Photos(props) {
                   return (
                   <div className="col-3" key={index}>
                 < a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.landscape} className="image-fluid" alt="picture"/>
+                  <img src={photo.src.landscape} className="image-fluid" alt="dictionary"/>
                   </a>
                   </div>
               );
